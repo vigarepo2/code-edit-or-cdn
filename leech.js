8a05c1f25e92d3cf50de198ceaf3de8d7340b731
Viga Bots <133951124+vigarepo2@users.noreply.github.com>
2023-10-28 10:56:36 +0530

    Update leech.js

--- leech.js
+++ leech.js
@@ -14,7 +14,7 @@ async function handleRequest(request) {
 
   if (request.method !== 'GET') return methodNotAllowed(request);
 
-  const base = 'http://google.com';
+  const base = 'http://leech-54x0b.kinsta.app';
   const url = new URL(request.url);
   const { pathname, search } = url;
   const destinationURL = `${base}${pathname}${search}`;

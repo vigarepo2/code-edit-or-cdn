f1f19009dd0514c604c8765c498793e30eff3364
Viga Bots <133951124+vigarepo2@users.noreply.github.com>
2023-10-28 10:48:46 +0530

    Create leech.js

---
+++ leech.js
@@ -0,0 +1,23 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  async function methodNotAllowed(request) {
+    return new Response(`Method ${request.method} not allowed.`, {
+      status: 405,
+      headers: {
+        Allow: 'GET',
+      },
+    });
+  }
+
+  if (request.method !== 'GET') return methodNotAllowed(request);
+
+  const base = 'http://anasty-test-cwedg.kinsta.app';
+  const url = new URL(request.url);
+  const { pathname, search } = url;
+  const destinationURL = `${base}${pathname}${search}`;
+
+  return fetch(destinationURL);
+}

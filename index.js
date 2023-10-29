30a1583fa8df3e9ac861dd1b03729199077656b0
Viga Bots <133951124+vigarepo2@users.noreply.github.com>
2023-10-29 12:44:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello, World!');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!');
+});
+
+const PORT = 8080; // Setting the server to run on port 8080
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}/`);
+});
